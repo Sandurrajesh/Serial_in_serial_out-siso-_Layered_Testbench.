67171e3fda5e8207cf9f38d38baffcec6b5c575a
Rajesh <150577409+Sandurrajesh@users.noreply.github.com>
2025-11-14 21:49:43 +0530

    Create my_pkg package with UVM includes

---
+++ pkg.sv
@@ -0,0 +1,21 @@
+package my_pkg;
+
+import uvm_pkg::*;
+`include "uvm_macros.svh"
+
+`include "packet.sv"
+`include "sequencer.sv"
+`include "sequence.sv"
+
+`include "driver.sv"
+`include "monitor.sv"
+`include "scoreboard.sv"
+`include "coverage.sv"
+`include "agent.sv"
+`include "env.sv"
+`include "test.sv"
+
+endpackage : my_pkg
+
+
+
